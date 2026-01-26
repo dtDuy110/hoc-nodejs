f5a16d2b5e1cc05a561722a5e3a17838213eaa5a
DuyGV <truongduyiuh222@gmail.com>
2026-01-26 17:10:28 +0700

    cc

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 8080;
 app.get("/", (req, res) => {
     res.send("hello world")
 })
 app.get("/duy", (req, res) => {
-    res.send("dang truonng duy")
+    res.send("dang truonnnnnnnng duy")
 })
 app.listen(8080, () => {
     console.log(`My app running on  ${port}`)
