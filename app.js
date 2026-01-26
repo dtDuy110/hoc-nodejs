90d4096fbc694c53c04106b7706e1f4ec3959323
DuyGV <truongduyiuh222@gmail.com>
2026-01-26 16:48:41 +0700

    24

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const express = require("express");
+const app = express();
+const port = 8080;
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+app.get("/duy", (req, res) => {
+    res.send("dang truonng duy")
+})
+app.listen(8080, () => {
+    console.log(`My app running on  ${port}`)
+})
