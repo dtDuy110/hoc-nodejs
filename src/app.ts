1c7286de145197bf21ac6c3db09466e52e1e0078
DuyGV <truongduyiuh222@gmail.com>
2026-01-26 17:48:40 +0700

    28

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 8080;
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send("hello world hihi")
 })
 app.get("/duy", (req, res) => {
     res.send("dang truonnnnnnnng duy")
